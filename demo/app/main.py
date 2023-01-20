d7a7665bf2d946fceedb0dc30e420fa69f3c1377
kidcoconut <iain.mckone@gmail.com>
2023-01-20 15:06:59 -0500

    first working version of displaying dataframe in browser

--- demo/app/main.py
+++ demo/app/main.py
@@ -1,18 +1,56 @@
-import fastapi
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+#from IPython.display import HTML
 import uvicorn
-import model as mdlClaims
+import lib.model as mdlClaims
+import lib.claims as libClaims
+#import pandas as pd
+#import json
+#import os
 
+from pathlib import Path
 
-api = fastapi.FastAPI()
+api = FastAPI()
+kstrBasePath = Path(__file__).resolve().parent
+templRef = Jinja2Templates(directory=str(kstrBasePath / "templ"))
 
+#print("INFO (basePath):  ", kstrBasePath)
+
+#--- get main ui/ux entry point
 @api.get('/')
 def index():
     return {
-        "message": "Hello World"
+        "message": "Hello World2"
     }
 
-@api.get('/claims/predict/')
+#--- get claims data; test; train; predict
+@api.get('/claims/train/')
+def get_claims_train():
+    #--- load raw csv(s)
+    #--- return json
+    #--- save as pandas data frame?
+    return {
+        "message": "get claims train data"
+    }
+
+@api.get('/claims/test/')
+@api.get('/claims/test/loadData/', response_class = HTMLResponse)
+def get_claims_test(request: Request, response: Response):
+    pdfClaims = libClaims.loadCsv_testData()
+    pdfSample = pdfClaims.sample(10)
+    htmlSample = pdfSample.to_html()
+
+    context = {'request': request, 'dataframe': pdfSample.to_html(classes='table table-stripped')}
+    result = templRef.TemplateResponse(
+        'templ_showDataframe.html', context)
+
+    return result
+
+@api.get('/api/claims/predict/xgb')
 def model_predict():
+    #--- input:  json claims data
     return {
         "message": "Model prediction"
     }
@@ -26,4 +64,5 @@ def tst_harness():
     }
 
 if __name__ == '__main__':
-    uvicorn.run(api)
+    uvicorn.run("main:api", host="0.0.0.0", port=48300, reload=True)
+#CMD ["uvicorn", "main:api", "--host=0.0.0.0", "--reload"]
