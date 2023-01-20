9e87432889769c36209aea445dd9b7018a23b710
kidcoconut <iain.mckone@gmail.com>
2023-01-20 17:59:02 -0500

    expanded api to include all test endpoints for EDA

--- demo/app/lib/model.py
+++ demo/app/lib/model.py
@@ -1,35 +1,53 @@
 import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
-from pickle import load, dump
+#from pickle import load, dump
+from pathlib import Path
+import pickle
+
+kstrBasePath = Path(__file__).resolve().parent.parent
+kstrMdlPath = str(kstrBasePath / "model/")
+
 
 #--- default methods
 def load():
-    return loadXGB()
+    return loadXGB_fromPkl()
 
 def train(pdfData):
     return trainXGB(pdfData)
 
-def predict(pdfData):
-    return predictXGB(pdfData)
+def predict(npaData):
+    return predictXGB(npaData)
 
 
 #--- XGBoost
-m_kstrMdlPath_gbt = '../model/gbt_model.pkl'
+m_kstrMdlPath_gbt = kstrMdlPath + '/gbt_model.pkl'
 def trainXGB(pdfData):
-    mdlAnoms = XGBoost()
+    mdlAnoms = GradientBoostingClassifier()
     mdlAnoms.fit(pdfData.values)
-    saveXGB(mdlAnoms)
+    saveXGB_toPkl(mdlAnoms)
     return mdlAnoms
 
-def loadXGB():
-    mdlAnoms = load(m_kstrMdlPath_gbt, 'rb')
+def loadXGB_fromPkl():
+    with open(m_kstrMdlPath_gbt, 'rb') as filPkl:
+        # load using pickle de-serializer
+        mdlAnoms = pickle.load(filPkl)
+
+    #mdlAnoms = load(m_kstrMdlPath_gbt, 'rb')
     return mdlAnoms
 
-def saveXGB(mdlAnoms):
-    dump(mdlAnoms, open(m_kstrMdlPath_gbt, 'wb'))
+def saveXGB_toPkl(mdlAnoms):
+    with open(m_kstrMdlPath_gbt, 'wb') as filPkl:
+        pickle.dump(mdlAnoms, filPkl)
+    #dump(mdlAnoms, open(m_kstrMdlPath_gbt, 'wb'))
     return mdlAnoms
 
-def predictXGB(pdfData):
-    mdlAnoms = loadXGB()
-    serPredict = mdlAnoms.predict(pdfData.values)  #--- Q:  is this a data series?
-    return serPredict
+def predictXGB(npaData):
+    #--- input:  numpy.ndarray of feature eng, and scaled data 
+    mdlAnoms = loadXGB_fromPkl()
+    npaPredict = mdlAnoms.predict(npaData)
+
+    #--- AttributeError: 'GradientBoostingClassifier' object has no attribute '_loss'
+    #--- version of scikit-learn?  Monika: ?.?.? ; Iain: 1.2.0
+
+    print("INFO (type.npaPredict):  ", type(npaPredict))
+    return npaPredict
