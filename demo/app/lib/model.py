545f720d7ae5c23b57d3ad057b1a333a750b5c25
kidcoconut <iain.mckone@gmail.com>
2023-01-22 18:37:50 -0500

    enabled loading of model.pkl, and generating a prediction

--- demo/app/lib/model.py
+++ demo/app/lib/model.py
@@ -12,45 +12,46 @@ kstrMdlPath = str(kstrBasePath / "model/")
 
 #--- default methods
 def load():
-    return loadXGB_fromPkl()
+    return loadGBC_fromPkl()
 
 def train(pdfData):
-    return trainXGB(pdfData)
+    return trainGBC(pdfData)
 
 def predict(npaData):
-    return predictXGB(npaData)
+    return predictGBC(npaData)
 
 
-#--- XGBoost
-m_kstrMdlPath_gbt = kstrMdlPath + '/gbt_model.pkl'
-def trainXGB(pdfData):
+#--- Supervised:  Gradient Boost Classifier
+m_kstrMdlPath_gbc = kstrMdlPath + '/gbc_model.pkl'
+def trainGBC(pdfData):
     mdlAnoms = GradientBoostingClassifier()
     mdlAnoms.fit(pdfData.values)
-    saveXGB_toPkl(mdlAnoms)
+    saveGBC_toPkl(mdlAnoms)
     return mdlAnoms
 
-def loadXGB_fromPkl():
-    with open(m_kstrMdlPath_gbt, 'rb') as filPkl:
+def loadGBC_fromPkl():
+    with open(m_kstrMdlPath_gbc, 'rb') as filPkl:
         # load using pickle de-serializer
-        #mdlAnoms = pickle.load(filPkl)
-        mdlAnoms = joblib.load(filPkl)
+        mdlAnoms = pickle.load(filPkl)
+        #mdlAnoms = joblib.load(filPkl)
 
     #mdlAnoms = load(m_kstrMdlPath_gbt, 'rb')
     return mdlAnoms
 
-def saveXGB_toPkl(mdlAnoms):
-    with open(m_kstrMdlPath_gbt, 'wb') as filPkl:
+def saveGBC_toPkl(mdlAnoms):
+    with open(m_kstrMdlPath_gbc, 'wb') as filPkl:
         pickle.dump(mdlAnoms, filPkl)
     #dump(mdlAnoms, open(m_kstrMdlPath_gbt, 'wb'))
     return mdlAnoms
 
-def predictXGB(npaData):
+def predictGBC(npaData):
     #--- input:  numpy.ndarray of feature eng, and scaled data 
-    mdlAnoms = loadXGB_fromPkl()
+    mdlAnoms = loadGBC_fromPkl()
     npaPredict = mdlAnoms.predict(npaData)
 
     #--- AttributeError: 'GradientBoostingClassifier' object has no attribute '_loss'
     #--- version of scikit-learn?  Monika: ?.?.? ; Iain: 1.2.0
 
-    print("INFO (type.npaPredict):  ", type(npaPredict))
+    #print("INFO (type.npaPredict):  ", type(npaPredict))
+    print("INFO (npaPredict.shape):  ", npaPredict.shape)
     return npaPredict
