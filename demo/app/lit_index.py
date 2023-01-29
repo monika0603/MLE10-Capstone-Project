cf632471817e2dd1d70d9d365b90555331415991
kidcoconut <iain.mckone@gmail.com>
2023-01-29 09:26:39 -0500

    removed sidebar select box

--- demo/app/lit_index.py
+++ demo/app/lit_index.py
@@ -2,20 +2,21 @@
     toExecute:  (from root app folder) ... streamlit run lit_index.py
 '''
 import streamlit as st
-from uix import lit_sidebar as lit_sideBar
+#from uix import lit_sidebar as lit_sideBar
+import uix.lit_sidebar as litSideBar
 
 
 #--- streamlit:  specify title and logo
 st.set_page_config(
-            page_title='Healthcare ML Claims Anomaly Detection:  ', 
-            page_icon='https://cdn.freebiesupply.com/logos/thumbs/1x/nvidia-logo.png', 
+            page_title='Healthcare ML Claims Anomaly Detection', 
+            #page_icon='https://cdn.freebiesupply.com/logos/thumbs/1x/nvidia-logo.png', 
             layout="wide")
-st.header("National Statistics")
+st.header("Healthcare ML Claims Anomaly Detection")
 st.markdown('---')
 
 
 #--- streamlit:  add a sidebar 
-lit_sideBar.init()
+litSideBar.init()
 
 
 #if __name__ == '__main__':
