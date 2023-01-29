c3c43b1593d11275f247b642456764b6f7aef499
kidcoconut <iain.mckone@gmail.com>
2023-01-29 09:12:04 -0500

    adjusting streamlit framework to be library based s file based

--- demo/app/lit_index.py
+++ demo/app/lit_index.py
@@ -1,37 +1,24 @@
 '''
     toExecute:  (from root app folder) ... streamlit run lit_index.py
 '''
-
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import datetime
-
-import pandas as pd
-
-import lib.claims as libClaims
-
-#--- get the dataframe
-pdfClaims = libClaims.loadPkl_testClaims()
+from uix import lit_sidebar as lit_sideBar
 
 
 #--- streamlit:  specify title and logo
-st.set_page_config(page_title='Healthcare ML Claims Anomaly Detection:  ', 
-page_icon='https://cdn.freebiesupply.com/logos/thumbs/1x/nvidia-logo.png', layout="wide")
+st.set_page_config(
+            page_title='Healthcare ML Claims Anomaly Detection:  ', 
+            page_icon='https://cdn.freebiesupply.com/logos/thumbs/1x/nvidia-logo.png', 
+            layout="wide")
+st.header("National Statistics")
+st.markdown('---')
 
 
 #--- streamlit:  add a sidebar 
-st.markdown('---')
-st.sidebar.image('https://cdn.freebiesupply.com/logos/thumbs/1x/nvidia-logo.png', width=200)
-st.sidebar.markdown('# Healthcare ML Claims Anomaly Detection')
-st.sidebar.markdown('Anomaly Detection, Automation is awesome ...')
-st.sidebar.markdown('Kaggle Claims Data - (imagine for 2022)')
-st.sidebar.markdown('Visualise Claims data Trends and Patterns over a given time span.') 
-
-st.sidebar.markdown('---')
-st.sidebar.write('Developed by Chavarria, McKone, Sharma')
-st.sidebar.write('Contact at iain.mckone@gmail.com')
+lit_sideBar.init()
 
 
-#rteClaims = APIRouter()
+#if __name__ == '__main__':
+#    st.run("main:app", host="0.0.0.0", port=48300, reload=True)
+
+#aryPkg[moduleNames.index(page)].run()
---
+++ demo/app/uix/pages/lit_about.py
@@ -0,0 +1,12 @@
+#--- about page
+import streamlit as st
+
+description = "About"
+def run():
+    #--- 
+    st.markdown(
+        """
+            About page
+        """,
+            unsafe_allow_html=True,
+        )
