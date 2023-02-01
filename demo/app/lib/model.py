cede429da571d8661a1e27201e503e3163b7d36f
kidcoconut <iain.mckone@gmail.com>
2023-02-01 17:11:55 -0500

    expanded model predictions and fastapi endpoints to include lgr, svm

--- demo/app/lib/model.py
+++ demo/app/lib/model.py
@@ -13,6 +13,10 @@ kstrMdlPath = str(kstrBasePath / "model/")
 m_kstrDataPath = libPaths.pth_data
 m_kstrModelPath = libPaths.pth_model
 
+#m_kstrMdlPath_gbc = kstrMdlPath + '/gbc_model.pkl'
+m_kstrMdlPath_gbc = kstrMdlPath + '/gbc_model_colab.pkl'
+m_kstrMdlPath_lgr = kstrMdlPath + '/lgr_model_colab.pkl'
+m_kstrMdlPath_svm = kstrMdlPath + '/svm_model_colab.pkl'
 
 #--- default methods
 def load():
@@ -45,8 +49,6 @@ def predictEnc(npaData):
 
 
 
-#--- Supervised:  Gradient Boost Classifier
-m_kstrMdlPath_gbc = kstrMdlPath + '/gbc_model.pkl'
 def fit_stdScaler(pdfData, blnIsTrain=False):
     #--- apply scaler
     #--- WARN:  scaling is also grouped by provider
@@ -54,15 +56,21 @@ def fit_stdScaler(pdfData, blnIsTrain=False):
 
     #--- note:  this is a numpy.ndarray
     #--- we need to fit the scaler, and then save as a pkl file
-    strScalerPath = m_kstrModelPath + "gbc_scaler.pkl"
+    strScalerPath = m_kstrModelPath + "std_scaler_colab.pkl"
     print("INFO (lib.model.fit_stdScalar):  ", strScalerPath)
     if (blnIsTrain):
         scaler = StandardScaler()
         sclFit = scaler.fit(pdfData)
+        #--- if we train locally;  write out to gbc_scalar.pkl
+        #--- we do not want to overwrite the colab version used for test
+        strScalerPath = m_kstrModelPath + "gbc_scaler.pkl"
+        print("WARN (lib.model.fit_stdScalar)  Using local pkl for Train: ", strScalerPath)
         with open(strScalerPath, 'wb') as filPkl:
             pickle.dump(sclFit, filPkl)
     else:
         #--- we need to load the pkl file
+        strScalerPath = m_kstrModelPath + "std_scaler_colab.pkl"
+        print("WARN (lib.model.fit_stdScalar)  Using colab pkl for Test: ", strScalerPath)
         with open(strScalerPath, 'rb') as filPkl:
             sclFit = pickle.load(filPkl)
     return sclFit
@@ -76,11 +84,13 @@ def fit_txfStdScaler(pdfData, blnIsTrain=False):
     return X_std
 
 
+#--- Supervised:  Gradient Boost Classifier
 def trainGBC(pdfData):
     mdlAnoms = GradientBoostingClassifier()
     mdlAnoms.fit(pdfData.values)
     saveGBC_toPkl(mdlAnoms)
     return mdlAnoms
+
 
 def loadGBC_fromPkl():
     with open(m_kstrMdlPath_gbc, 'rb') as filPkl:
@@ -91,11 +101,15 @@ def loadGBC_fromPkl():
     #mdlAnoms = load(m_kstrMdlPath_gbt, 'rb')
     return mdlAnoms
 
+
+
 def saveGBC_toPkl(mdlAnoms):
     with open(m_kstrMdlPath_gbc, 'wb') as filPkl:
         pickle.dump(mdlAnoms, filPkl)
     #dump(mdlAnoms, open(m_kstrMdlPath_gbt, 'wb'))
     return mdlAnoms
+
+
 
 def predictGBC(npaData):
     #--- input:  numpy.ndarray of feature eng, and scaled data 
@@ -106,5 +120,39 @@ def predictGBC(npaData):
     #--- version of scikit-learn?  Monika: ?.?.? ; Iain: 1.2.0
 
     #print("INFO (type.npaPredict):  ", type(npaPredict))
+    print("INFO (npaPredict.shape):  ", npaPredict.shape)
+    return npaPredict
+
+
+
+#--- Supervised:  Logistic Regession
+def loadLogR_fromPkl():
+    with open(m_kstrMdlPath_lgr, 'rb') as filPkl:
+        mdlAnoms = pickle.load(filPkl)
+    return mdlAnoms
+
+
+def predictLogR(npaData):
+    #--- input:  numpy.ndarray of feature eng, and scaled data 
+    mdlAnoms = loadLogR_fromPkl()
+    npaPredict = mdlAnoms.predict(npaData)
+
+    print("INFO (npaPredict.shape):  ", npaPredict.shape)
+    return npaPredict
+
+
+
+#--- Supervised:  Support Vector Machines
+def loadSVM_fromPkl():
+    with open(m_kstrMdlPath_svm, 'rb') as filPkl:
+        mdlAnoms = pickle.load(filPkl)
+    return mdlAnoms
+
+
+def predictSVM(npaData):
+    #--- input:  numpy.ndarray of feature eng, and scaled data 
+    mdlAnoms = loadSVM_fromPkl()
+    npaPredict = mdlAnoms.predict(npaData)
+
     print("INFO (npaPredict.shape):  ", npaPredict.shape)
     return npaPredict
