012ea3c6f1dd1a7140cf7cd0dbdcd499ee6e0701
kidcoconut <iain.mckone@gmail.com>
2023-02-10 11:49:31 -0500

    qa:  solving pkl incompat issue;  reset env to 3.8.16; using highest pkl protocol; sklearn 1.1.1

--- demo/app/uix/pages/lit_about.py
+++ demo/app/uix/pages/lit_about.py
@@ -3,7 +3,12 @@ import streamlit as st
 
 description = "About"
 def run():
+
+    print("\nINFO (lit_about.run)  loading ", description, " page ...") 
+
     #--- 
+    st.experimental_memo.clear()            #--- try to clear cache each time this page is hit
+
     st.markdown('### About')
     st.markdown('<why this is important> ...')
     st.markdown('Kaggle Claims Data - (assume 2022)')
