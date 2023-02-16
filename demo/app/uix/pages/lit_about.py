364fbfda334d81eab9939a39ad92143528224a73
kidcoconut <iain.mckone@gmail.com>
2023-02-16 17:24:16 -0500

    cleaned up dir structure;  updated preso;  updated streamlit;  added model screenshots

--- demo/app/uix/pages/lit_about.py
+++ demo/app/uix/pages/lit_about.py
@@ -6,13 +6,13 @@ def run():
 
     print("\nINFO (lit_about.run)  loading ", description, " page ...") 
 
-    #--- 
-    #st.experimental_memo.clear()            #--- try to clear cache each time this page is hit
-    #st.cache_data.clear()
 
     st.markdown('### About')
-    st.markdown('<why this is important> ...')
-    st.markdown('Kaggle Claims Data - (assume 2022)')
+    st.markdown('### MLE10 Capstone:  Healthcare Anomaly Detection')
+    st.markdown('#### Team:  McKone, Sharma, Chavarria, Lederer')
+
+    st.markdown('Kaggle Claims Data:')
+    st.markdown('https://www.kaggle.com/code/rohitrox/medical-provider-fraud-detection/data')
     st.markdown(
         """
             About page
